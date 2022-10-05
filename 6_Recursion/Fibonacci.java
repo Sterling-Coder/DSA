9be213e181d0173f270049b30bf1b955c00d6249
Aman Rastogi <115058450+Sterling-Coder@users.noreply.github.com>
2022-10-06 01:41:21 +0530

    Create Fibonacci.java

---
+++ 6_Recursion/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    static int FibonacciNum(int n) {
+        int fibonacci;
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        fibonacci = FibonacciNum(n-1) + FibonacciNum(n - 2);
+        return fibonacci;
+    }
+    public static void main(String[] args) {
+        int n = 25;
+        FibonacciNum(n); // SC-> O(n)  TC->  O(2^n)
+    }
+}
