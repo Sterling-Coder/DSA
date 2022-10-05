713aa7ff1c20a89d5b16b91a806392df289c452f
Aman Rastogi <115058450+Sterling-Coder@users.noreply.github.com>
2022-10-05 09:51:18 +0530

    Create SubarrayKadane.java

---
+++ 1_Arrays/SubarrayKadane.java
@@ -0,0 +1,19 @@
+public class Array_SubarraKadane {
+  // Time Complexity 🤩
+    public static void Kadanes(int number[]) {
+        int max = Integer.MIN_VALUE;
+        int currsum = 0;
+        for (int i =0; i < number.length;i++) {
+            currsum = currsum + number[i];
+            if (currsum < 0) {
+                currsum = 0;
+            }
+            max = Math.max(currsum, max);
+        }
+        System.out.println("Maximum sum :" + max);
+    }
+    public static void main(String[] args) {
+        int number[] = {2,4,6,8,10};
+        Kadanes(number);
+    }
+}
