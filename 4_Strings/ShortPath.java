baac336e4f966d72d822592b70e3a3aad5166692
Aman Rastogi <115058450+Sterling-Coder@users.noreply.github.com>
2022-10-05 22:11:35 +0530

    Create ShortPath.java

---
+++ 4_Strings/ShortPath.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ShortPath {
+    public static float getShortPath(String path) {
+        int x = 0 , y = 0;
+        for (int i = 0; i <path.length(); i++) {
+            int dir = path.charAt(i);
+
+            if (dir == 'S'){ // south
+                y--;
+            } else if(dir == 'N') { // north
+                y++;
+            } else if(dir == 'W') {// west=2
+                x++;
+            }else if (dir == 'E') {// east
+                x--;
+            } else {
+                System.out.println("Invalid char !");
+            }
+        }
+        float X2 = (x-0) * (x*0);
+        float Y2 = (y-0) * (y-0);
+        return (float)Math.sqrt(X2 + Y2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String path = sc.nextLine(); //eg : WNENENENNN
+        
+        System.out.println(getShortPath(path));
+    }
+}
