5748a30b056e6f4a62b2a1b15d9e8a03736dd782
Aman Rastogi <115058450+Sterling-Coder@users.noreply.github.com>
2022-10-13 19:43:15 +0530

    Create Subset.java

---
+++ 8_BackTracking/Subset.java
@@ -0,0 +1,20 @@
+public class BackTracStr {
+    public static void SubSet(String str, String ans, int i ) {
+      //Base Case
+        if (i == str.length() ) {    // To show empty set to null
+          //  if (ans.length() == 0) {
+          //      System.out.println("null");
+          //  }else {
+                System.out.println(ans);
+          //  }
+            return;
+        }
+      //Recursion
+        SubSet(str,ans+str.charAt(i),i+1); // Yes
+        SubSet(str, ans, i+1); // No
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        SubSet(str, "",0);
+    }
+}
